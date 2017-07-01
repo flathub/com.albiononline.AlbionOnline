041455e1478b379d2eff0acbcad38ac3856a318d
Valentin David <valentin.david@gmail.com>
2017-07-01 20:22:23 +0200

    Fix sound. Plugin's libSDL2 needed to be forced through LD_PRELOAD.

--- albion-online.sh
+++ albion-online.sh
@@ -5,5 +5,6 @@ if ! [ -x "${HOME}/.albiononline/launcher/Albion-Online" ]; then
   cp -r /app/extra/data/launcher "${HOME}/.albiononline/"
 fi
 cd "${HOME}/.albiononline/"
+export LD_PRELOAD="${HOME}/.albiononline/game_x64/Albion-Online_Data/Plugins/x86_64/libSDL2-2.0.so.0"
 export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:${HOME}/.albiononline/launcher"
 exec "${HOME}/.albiononline/launcher/Albion-Online"
