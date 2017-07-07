afdefcf08714f6492c8320b8b42ef3fe34bbd839
Valentin David <valentin.david@gmail.com>
2017-07-07 21:16:27 +0200

    Verify home copy of launcher whether it matches the /app/extra version. Re-copy if not.

--- albion-online.sh
+++ albion-online.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
-if ! [ -x "${HOME}/.albiononline/launcher/Albion-Online" ]; then
-  mkdir -p "${HOME}/.albiononline"
-  cp -r /app/extra/data/launcher "${HOME}/.albiononline/"
+available_verion="$(cat /app/extra/data/launcher/version.txt)"
+home_version="$(cat "${HOME}/.albiononline/launcher/version.txt" 2>/dev/null)"
+if [ "${home_version}" != "${available_version}" ]; then
+    mkdir -p "${HOME}/.albiononline"
+    rm -rf "${HOME}/.albiononline/launcher"
+    cp -r /app/extra/data/launcher "${HOME}/.albiononline/"
 fi
 cd "${HOME}/.albiononline/"
 export LD_PRELOAD="${HOME}/.albiononline/game_x64/Albion-Online_Data/Plugins/x86_64/libSDL2-2.0.so.0"
