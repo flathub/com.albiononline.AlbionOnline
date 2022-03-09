a2b33f81a204459c79809537aae828be2762007c
Valentin David <me@valentindavid.com>
2022-03-09 10:10:08 +0100

    Do not copy xpm icon
    This causes an error in apply_extra

--- apply_extra.sh
+++ apply_extra.sh
@@ -7,6 +7,3 @@ unzip albion-online-setup.zip 'data/*'
 mv data/* .
 rm -rf data
 rm albion-online-setup
-
-mkdir -p export/share/icons/hicolor/128x128/apps
-cp AlbionOnline.xpm export/share/icons/hicolor/128x128/apps/com.albiononline.AlbionOnline.xpm
